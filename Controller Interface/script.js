90473e725f7db0a0216c75b44b594a513121e87b
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-06 20:21:28 +0200

    .

--- Controller Interface/script.js
+++ Controller Interface/script.js
@@ -1,1 +1,11 @@
 // JavaScript source code
+function updateX() {
+    console.log("updateX");
+    document.getElementById('xline').style.left = document.getElementById('myRangeX').value;
+}
+
+function updateY() {
+    console.log("updateY");
+    document.getElementById('yline').style.top = document.getElementById('myRangeY').value;
+}
+
