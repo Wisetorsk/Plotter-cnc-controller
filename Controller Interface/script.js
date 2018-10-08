110e5b1b6e9d6bb7e91f42b81d60771df8278842
Marius Kristiansen <43368859+Wisetorsk@users.noreply.github.com>
2018-10-08 20:30:47 +0200

    added x and y job buttons. + State display

--- Controller Interface/script.js
+++ Controller Interface/script.js
@@ -9,3 +9,39 @@ function updateY() {
     document.getElementById('yline').style.top = document.getElementById('myRangeY').value;
 }
 
+function xjog(element) {
+    console.log('x:');
+    if (element.value == "right") {
+        console.log('jog right');
+    } else {
+        console.log('jog left');
+    }
+}
+
+function yjog(element) {
+    console.log('Y:');
+    if (element.value == "right") {
+        console.log('jog right');
+    } else {
+        console.log('jog left');
+    }
+}
+
+function getMachineStatus() {
+    console.log('Fetching current machine state');
+    document.getElementById('linkText').innerHTML = "Inactive";
+}
+
+function main() {
+    getMachineStatus();
+}
+
+function home() {
+    console.log('Going Home');
+}
+
+function goTo(x, y) {
+    console.log('Moving to (' + x + ',' + y + ')');
+}
+
+main();
